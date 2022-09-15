9d4384a9600bc458600c105818b74608fa0f2adf
Edson <edson.cs.dev@gmail.com>
2022-09-14 22:22:17 -0300

    print module

---
+++ print/print.go
@@ -0,0 +1,39 @@
+package adapters
+
+import (
+	"go-pocketbase/pkg/adapters-log"
+	"sync"
+)
+
+type Log struct {
+}
+
+var once sync.Once
+
+func (z Log) New() adapters.Log {
+	return z
+}
+
+func (z Log) SetLevel(level string) {
+}
+func (z Log) GetLevel() string {
+	return ""
+}
+func (z Log) Throw(err error) {
+	println(err.Error())
+}
+func (z Log) Debug(message string) {
+	println(message)
+}
+func (z Log) Info(message string) {
+	println(message)
+}
+func (z Log) Warn(message string) {
+	println(message)
+}
+func (z Log) Error(message string) {
+	println(message)
+}
+func (z Log) Fatal(message string) {
+	println(message)
+}
